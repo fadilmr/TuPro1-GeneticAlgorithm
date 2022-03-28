f3728f1d3d6958f1fe4c058939108f608975235e
Muhammad Fadil Maulana Akbar <m.muhammadfadil12@gmail.com>
2022-03-28 13:21:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,61 @@
 import random
 import math
 
+class GA:
+    def __init__(self):
+        pass
 
-def generateChromosome(chr_size):
-    hasil =[]
+    def generateChromosome(self):
+        hasil =[]
+            #looping sebanyak chr_size
+        for _ in range(self):
+            hasil.append(random.randint(0, 1))
+        return  hasil
 
-        #looping sebanyak chr_size
-    for _ in range(chr_size):
-        hasil.append(random.randint(0, 1))
-    return  hasil
+    def decodeChromosome(self):
+        xMin, xMax = (-5, 5)
+        yMin, yMax = (-5, 5)
 
-def decodeChromosome(chromosome, chr_size):
-    xMin, xMax = (-5, 5)
-    yMin, yMax = (-5, 5)
+        N, x, y = 0, 0, 0
+        n = (self) // 2
 
-    N, x, y = 0, 0, 0
-    n = (chr_size) // 2
+        for i in range(0, n):
+            N += 2**-(i+1)
+        for j in range(0, n):
+            x += self[i] * 2 ** -(i+1)
+            y += self[n + i] * 2 ** -(i+1)
+        x = xMin + (((xMax - xMin) / N) * x) 
+        y = yMin + (((yMax - yMin) / N) * y) 
+            
+        return [x, y]
 
-    for i in range(0, n):
-        N += 2**-(i+1)
-    for j in range(0, n):
-        x += chromosome[i] * 2 ** -(i+1)
-        x += chromosome[n + i] * 2 ** -(i+1)
-    x = xMin + (((xMax - xMin) / N) * x) 
-    y = yMin + (((yMax - yMin) / N) * y) 
-        
-    return [x, y]
+    def fungsi(self):
+         return ((math.cos(self)+math.sin(self))**2) / (self**2 + self**2)
 
+    def fitness(self):
+        hasil = []
+         
+        for i in self:
+            hasil.append(self)
+        pass
+
+    def touramentSelection(self):
+        pass
+    
+    def crosover(self):
+        pass
+
+    def mutasi(self):
+        pass
+
+    def seleksiSurvivor(self):
+        pass
+
+    def perpindahanGenerasi(self):
+        pass
+
+    def printGA():
+        pass
 
 
 
